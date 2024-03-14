983a3eac30353782192a012771f9240d6783873c
totallyepichackerman <rschwartz25@pascack.org>
2024-03-14 10:35:03 -0400

    made everthing actually functional

--- script/script.js
+++ script/script.js
@@ -2,35 +2,41 @@ let pageCount = 3;
 let currentPage = 0;
 let pages;
 
+let pageCountEl = document.getElementById('total-pages');
+let pageNumberEl = document.getElementById('current-page');
+
 initializePages();
 
-function initializePages(){
-    pages = document.getElementsByClassName("page");
+function initializePages() {
+    pages = document.getElementsByClassName('page');
+    pageCountEl.textContent = pageCount;
 }
 
-function nextPage(){
-    if(currentPage +1 >=pageCount)
+function nextPage() {
+    if(currentPage + 1 >= pageCount)
         return;
 
     pages[currentPage].classList.toggle('hidden');
-
+    
     currentPage++;
 
     pages[currentPage].classList.toggle('hidden');
+    pageNumberEl.textContent = currentPage + 1;
 }
 
-function prevPage(){
-    if(currentPage -1 < 0)
+function prevPage() {
+    if(currentPage - 1 < 0)
         return;
 
     pages[currentPage].classList.toggle('hidden');
-
+    
     currentPage--;
-
+    
     pages[currentPage].classList.toggle('hidden');
+    pageNumberEl.textContent = currentPage + 1;
 }
 
-function toggleNav(){
+function toggleNav() {
     let navbar = document.getElementById('sidebar-nav');
-    navbar.classList.toggle('hidden');
+    navbar.classList.toggle('sidebar-shown');
 }
